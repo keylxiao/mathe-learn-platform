4675b351884a5023ed07d5edc828368a703506c4
keylxiao <1285843834@qq.com>
2023-01-30 16:55:41 +0800

    chore :wrench: : 更新外部网站数据表创建文件

--- createsql.sql
+++ createsql.sql
@@ -5,6 +5,21 @@ DROP TABLE IF EXISTS `home_page_informations`;
 CREATE TABLE `home_page_informations`
 (
     `total_visitor` int DEFAULT 0 COMMENT '网站总访问人次'
-)charset utf8 collate utf8_general_ci;
+) charset utf8
+  collate utf8_general_ci;
 INSERT INTO `home_page_informations`
 VALUES (1);
+
+-- ----------------------------
+-- 外部网站数据表
+-- ----------------------------
+DROP TABLE IF EXISTS `outside_webs`;
+CREATE TABLE `outside_webs`
+(
+    `name`        varchar(20) DEFAULT NULL COMMENT '网站内容',
+    `link`        varchar(50) DEFAULT NULL COMMENT '网址',
+    `class`       varchar(15) DEFAULT NULL COMMENT '学科门类',
+    `picture_id`  int DEFAULT 0 COMMENT '图标id',
+    `brief_intro` varchar(20) DEFAULT NULL COMMENT '简介'
+) charset utf8
+  collate utf8_general_ci;
