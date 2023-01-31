0548dacd4569e1c5200168b9c739511fcdb3528a
keylxiao <1285843834@qq.com>
2023-01-31 16:02:49 +0800

    fix :bug: : 修改用户结构体

--- models/user.go
+++ models/user.go
@@ -1,15 +1,15 @@
 package models
 
 type User struct {
-    Id         int    `gorm:"primary_key;auto_increment"` // 自增主键
-    Account    string // 登录账号
+    Id         string `gorm:"primary_key"` // 主键
+    Account    string // 学号/工号
     Password   string // 密码(sha1加密)
     UserName   string // 用户名
-    TelePhone  string // 电话号码
+    Telephone  string // 电话号码
+    QQNumebr   string // QQ号
     Status     int    // 用户权限(0平台管理员 1老师 2学生)
     College    int    // 学院
     Major      string // 专业
     CreateTime string // 创建时间
     UpdateTime string // 修改时间
-    IsDelete   int    // 逻辑删除(0未删 1删除)
 }
