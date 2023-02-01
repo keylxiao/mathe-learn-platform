6fad80805b607bc969ca982e6deb0be2a8023cc7
keylxiao <1285843834@qq.com>
2023-02-01 13:53:18 +0800

    fix :bug: : 修复了用户上传头像接口

--- controllers/user.go
+++ controllers/user.go
@@ -2,21 +2,30 @@ package controllers
 
 import (
     "github.com/kataras/iris/v12"
-    "mime/multipart"
+    "io"
     "net/http"
+    "os"
 )
 
 // PostLoadImage 上传用户头像
 func PostLoadImage(c iris.Context) {
     userId := c.URLParam("id")
-    _, err := c.UploadFormFiles("./UserImage", func(ctx iris.Context, file *multipart.FileHeader) {
-        file.Filename = userId
-    })
+    file, _, err := c.FormFile("image")
     if err != nil {
-        c.StatusCode(http.StatusBadRequest)
+        c.StatusCode(http.StatusInternalServerError)
         c.JSON("上传失败")
-    } else {
-        c.StatusCode(http.StatusOK)
-        c.JSON("上传成功")
+        return
     }
+    defer file.Close()
+    userId = userId + ".jpg"
+    out, err := os.OpenFile("./UserImage/"+userId, os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        c.StatusCode(http.StatusInternalServerError)
+        c.JSON("上传失败")
+        return
+    }
+    defer out.Close()
+    io.Copy(out, file)
+    c.StatusCode(http.StatusOK)
+    c.JSON("上传成功")
 }
