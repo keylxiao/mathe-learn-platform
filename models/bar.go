b38879987632b876f4b86f45db55cb686c143a2e
keylxiao <1285843834@qq.com>
2023-02-16 12:15:25 +0800

    feat :sparkles: : 更新贴子信息结构体

---
+++ models/bar.go
@@ -0,0 +1,13 @@
+package models
+
+// Bar 帖子信息
+type Bar struct {
+    Id          string // 帖子id
+    UserId      string // 用户id
+    Name        string // 帖子名称
+    Brief       string // 简介
+    CreateTime  string // 创建时间
+    FloorNumber int    // 总盖楼数
+    PageView    int    // 浏览量
+    LikesNuber  int    // 点赞数
+}
