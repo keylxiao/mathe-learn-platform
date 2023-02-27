efa56e4cdab7adbbb2baf647f26729dc4597071b
keylxiao <1285843834@qq.com>
2023-02-27 21:27:03 +0800

    feat :sparkles: : 更新楼层&楼中楼信息结构体

--- models/bar.go
+++ models/bar.go
@@ -11,3 +11,23 @@ type Bar struct {
     PageView    int    // 浏览量
     LikesNumber int    // 点赞数
 }
+
+// BarFloor 楼层信息
+type BarFloor struct {
+    Id             string // 楼层id
+    UserId         string // 用户id
+    CreateTime     string // 创建时间
+    FloorNumber    int    // 楼层数
+    LikesNumber    int    // 点赞数
+    SonFloorNumber int    // 楼中楼数
+}
+
+// SonFloor 楼中楼信息
+type SonFloor struct {
+    Id          string // 楼中楼id
+    Userid      string // 用户id
+    ReplyId     string // 回复楼中楼id, 空则为回复所在楼层
+    CreateTime  string // 创建时间
+    FloorNumber int    // 楼中楼楼层数
+    LikesNumber int    // 点赞数
+}
